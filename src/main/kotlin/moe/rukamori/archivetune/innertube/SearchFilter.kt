e1199171785e0085a334efbbf08989bd843da98b
rukamori <107134739+rukamori@users.noreply.github.com>
2026-09-16 23:16:04 +0700

    feat(SearchFilter): add FILTER_PODCAST constant

--- src/main/kotlin/moe/rukamori/archivetune/innertube/SearchFilter.kt
+++ src/main/kotlin/moe/rukamori/archivetune/innertube/SearchFilter.kt
@@ -9,6 +9,7 @@ value class SearchFilter(
         val FILTER_VIDEO = SearchFilter("EgWKAQIQAWoKEAkQChAFEAMQBA%3D%3D")
         val FILTER_ALBUM = SearchFilter("EgWKAQIYAWoKEAkQChAFEAMQBA%3D%3D")
         val FILTER_ARTIST = SearchFilter("EgWKAQIgAWoKEAkQChAFEAMQBA%3D%3D")
+        val FILTER_PODCAST = SearchFilter("EgWKAQJQAWoKEAkQChAFEAMQBA%3D%3D")
         val FILTER_FEATURED_PLAYLIST = SearchFilter("EgeKAQQoADgBagwQDhAKEAMQBRAJEAQ%3D")
         val FILTER_COMMUNITY_PLAYLIST = SearchFilter("EgeKAQQoAEABagoQAxAEEAoQCRAF")
     }
